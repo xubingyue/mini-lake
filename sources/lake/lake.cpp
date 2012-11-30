466c2a02d5f7dad896879ac96b9c7ee50ee6dd3c
yayanyang <yayanyang@gmail.com>
2012-11-30 18:08:52 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

---
+++ sources/lake/lake.cpp
@@ -0,0 +1,14 @@
+/**
+* 
+* @file     lake
+* @brief    Copyright (C) 2012  yayanyang All Rights Reserved 
+* @author   yayanyang
+* @version  1.0.0.0  
+* @date     2012/11/30
+*/
+
+
+int main(int args, char ** argv)
+{
+	args; argv;
+}
